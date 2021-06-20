9bdaf3bd075da94b66a0340b023e24c2c68efe37
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-19 21:45:01 -0300

    add LiteCombatCommand.java

---
+++ src/dev/_2lstudios/litecombat/commands/LiteCombatCommand.java
@@ -0,0 +1,12 @@
+package dev._2lstudios.litecombat.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class LiteCombatCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        return true;
+    }
+}
