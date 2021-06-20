05f3e9b5b42aeee97723ce48d0662a9def74b3d6
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-19 21:44:52 -0300

    add LiteCombat.java

---
+++ src/dev/_2lstudios/litecombat/LiteCombat.java
@@ -0,0 +1,10 @@
+package dev._2lstudios.litecombat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LiteCombat extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
